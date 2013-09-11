c8b0cac035b4ccce2c5cebb6dc5cf5e8a0af0f10
Rich Parrish <tradename@gmail.com>
2013-09-11 14:55:20 -0700

    still some issues

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,24 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
-};
+
+	var tracker = [];
+		
+	var main = arguments[1] || document.body;
+	if(main.hasChildNodes()){
+		var arr = main.childNodes;
+		for (var i in arr){
+			if(typeof arr[i] === 'object' && arr[i].classList.contains(className)){  
+				var m = getElementsByClassName(className,arr[i]);	
+				if (m !== undefined) tracker.push(m);
+			}
+		}					
+	}
+	else {
+		var cl = arguments[1].classList;
+		if(typeof cl === 'object' && cl.contains(className)){
+			return(arr[i]);
+		}
+	}
+}
+	
