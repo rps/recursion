ab407a2224517e686c0a1908cef1cf8f6e5d1dcc
Hack Reactor Students <github@hackreactor.com>
2013-09-23 16:29:03 -0700

    null, boolean, number, limited arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,25 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  var first = json.slice(0,1);
+  if(json === "null"){
+    return null;
+  }
+  else if (json === 'true'|| json === 'false'){
+    return (json === 'true') ? true : false;
+  }
+  else if (!isNaN(+json)){
+    return +json;
+  }
+  else if (first === '"'){
+    return json.slice(1,json.length-1);
+  }
+  else if (first === "["){
+    if(json === '[]') return [];
+    
+    // test out a for loop to walk along looking for [
+    return (json.slice(1,json.length-1).split(',')).map(function(item){
+      return parseJSON(item);
+    });
+  }
 };
