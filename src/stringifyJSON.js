9b034c8ab24917cc85a20684dee2f4e78931fdb2
Hack Reactor Students <github@hackreactor.com>
2013-09-23 15:44:09 -0700

    stringifyJSON complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,46 +3,22 @@
 // but you don't so you're going to have to write it from scratch:
 
 var stringifyJSON = function (obj) {
-	var ans = "";
-	if(typeof obj === undefined){
-		return;
-	}
-	else if (obj === null){
-		return 'null';
-	}
-	else if(typeof obj === 'object'){	
-	//array
-		if(Array.isArray(obj)){
-			if(obj.length === 0){
-				return '[]';
-			}
-			ans = ans.concat('[');
-			for(var i = 0;i<obj.length;i++){
-				ans = ans.concat(stringifyJSON(obj[i])+',');
-			}
-			return ans.slice(0,-1).concat(']');
-		}
-	//object or null
-		else {
-			if(Object.keys(obj).length === 0) return '{}';
-			ans = ans.concat('{');
-			for (var keys in obj){
-				ans = ans.concat(stringifyJSON(keys)+":"+stringifyJSON(obj[keys])+',')
-			}
-			if(ans.indexOf('undefined')>-1){
-				return '{}';
-			}
-			return ans.slice(0,-1).concat('}');
-		}
-	}
-	else if(typeof obj === 'string'){
-		return ('"'+obj+'"');
-	}
-	else if (typeof obj === 'boolean' || typeof obj === 'number'){
-		return String(obj);
-	}
-	else {
-		return;
-	}
-	
-}
+  if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj)){
+    return '[' + obj.map(function(val){
+      return stringifyJSON(val);
+    }).join(',') + ']';
+  }
+  else if (obj && typeof obj === 'object'){
+    var ans = [];
+    for (var key in obj){
+      if (obj[key] !== undefined && typeof obj[key] !== 'function'){
+        ans.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + ans.join(',') + '}';
+  }
+  return obj + '';
+};
